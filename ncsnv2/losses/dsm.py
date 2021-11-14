af70722b1cddfc3dd253ce095b8327737438ec9e
mariusarvinte <mariusarvinte@gmail.com>
2021-11-14 17:46:26 -0600

    Add files via upload
    NCSNv2 network from https://github.com/ermongroup/ncsnv2. All credit goes to original authors

---
+++ ncsnv2/losses/dsm.py
@@ -0,0 +1,33 @@
+import torch
+
+def anneal_dsm_score_estimation(scorenet, samples, sigmas,
+                                labels=None, anneal_power=2., hook=None):
+    # This always enters during training
+    if labels is None:
+        # Randomly sample sigma
+        labels = torch.randint(0, len(sigmas), 
+                   (samples.shape[0],), device=samples.device)
+    
+    used_sigmas = sigmas[labels].view(samples.shape[0], * ([1] * len(samples.shape[1:])))
+    noise       = torch.randn_like(samples) * used_sigmas
+    
+    perturbed_samples = samples + noise
+    
+    # Desired output
+    target = - 1 / (used_sigmas ** 2) * noise
+    
+    # Actual output
+    scores = scorenet(perturbed_samples, labels)
+    
+    # L2 regression
+    target = target.view(target.shape[0], -1)
+    scores = scores.view(scores.shape[0], -1)
+    
+    # Multiply each sample by its weight
+    loss = 1 / 2. * ((scores - target) ** 2).sum(
+        dim=-1) * used_sigmas.squeeze() ** anneal_power
+
+    if hook is not None:
+        hook(loss, labels)
+
+    return loss.mean(dim=0)
