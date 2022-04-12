d9d369fe26bd1380534be66644a389ff95c3f0dc
mariusarvinte <5852612+mariusarvinte@users.noreply.github.com>
2022-04-11 19:47:56 -0500

    Update hyperparam_tuning.py

--- hyperparam_tuning.py
+++ hyperparam_tuning.py
@@ -198,4 +198,6 @@ for meta_idx, (spacing, pilot_alpha, step_factor) in tqdm(enumerate(meta_params)
 # Squeeze
 oracle_log = np.squeeze(oracle_log)
 # Plot average NMSE at best stopping point and step factor value
-plt.figure(); plt.plot(10*np.log10(np.min(np.mean(oracle_log, axis=-1), axis=(-1, -3)))); plt.show()
+plt.figure(); plt.plot(snr_range, 
+       10*np.log10(np.min(np.mean(oracle_log, axis=-1), axis=(-1, -3))));
+plt.xlabel('SNR [dB]'); plt.ylabel('NMSE [dB]'); plt.grid(); plt.show()
